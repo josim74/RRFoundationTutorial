2a385c82280085ff49ca401b069aa77bc72597ad
Md. Josim Uddin <mdjosimuddin712@gmail.com>
2017-01-08 21:41:58 +0600

    MainSecond.java file is created

---
+++ MainSecond.java
@@ -0,0 +1,13 @@
+package comjosim74.github.rrfoundationtutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_second);
+    }
+}
